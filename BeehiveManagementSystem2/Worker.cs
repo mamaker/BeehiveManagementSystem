98a1059e513fd47f8e44fb5fbf8a324797a1e19f
mamaker <madhuvasudevan@hotmail.com>
2015-05-09 01:15:09 -0500

    Push all

---
+++ BeehiveManagementSystem2/Worker.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeehiveManagementSystem2
+{
+    class Worker : Bee
+    {
+        private string[] jobsICanDo;
+        private int shiftsToWork;
+        private int shiftsWorked;
+
+        public Worker(string[] jobsICanDo, double weightMg)
+            : base(weightMg)
+        {
+            this.jobsICanDo = jobsICanDo;
+        }
+
+        const double honeyUnitsPerShiftWorked = .65;
+        public override double HoneyConsumptionRate()
+        {
+            double consumption = base.HoneyConsumptionRate();
+            consumption += shiftsWorked * honeyUnitsPerShiftWorked;
+            return consumption;
+        }
+
+        public int ShiftsLeft
+        {
+            get
+            {
+                return shiftsToWork - shiftsWorked;
+            }
+        }
+
+        private string currentJob = "";
+        public string CurrentJob
+        {
+            get
+            {
+                return currentJob;
+            }
+        }
+
+        public bool DoThisJob(string job, int numberOfShifts)
+        {
+            if (!String.IsNullOrEmpty(currentJob))
+                return false;
+            for (int i = 0; i < jobsICanDo.Length; i++)
+                if (jobsICanDo[i] == job)
+                {
+                    currentJob = job;
+                    this.shiftsToWork = numberOfShifts;
+                    shiftsWorked = 0;
+                    return true;
+                }
+            return false;
+        }
+
+        public bool DidYouFinish()
+        {
+            if (String.IsNullOrEmpty(currentJob))
+                return false;
+            shiftsWorked++;
+            if (shiftsWorked > shiftsToWork)
+            {
+                shiftsWorked = 0;
+                shiftsToWork = 0;
+                currentJob = "";
+                return true;
+            }
+            else
+                return false;
+        }
+
+
+    }
+}
