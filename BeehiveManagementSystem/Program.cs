c0c069842d532f7956ae9e4d1d821236efb72e21
mamaker <madhuvasudevan@hotmail.com>
2015-05-09 01:09:59 -0500

    Made BeehiveManagementSystem2

--- BeehiveManagementSystem/Program.cs
+++ BeehiveManagementSystem/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace BeehiveManagementSystem
+namespace BeehiveManagementSystem2
 {
     static class Program
     {
--- BeehiveManagementSystem/Queen.cs
+++ BeehiveManagementSystem/Queen.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BeehiveManagementSystem
+namespace BeehiveManagementSystem2
 {
     class Queen
     {
